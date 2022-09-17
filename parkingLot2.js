959f3941a708489016d4bac952e8982b042a4c15
Okoli Evans <mr.evans0075@gmail.com>
2022-09-17 10:31:28 +0100

    edit js

---
+++ parkingLot2.js
@@ -0,0 +1,30 @@
+let carSlots = document.getElementsByClassName('btnSmall')
+let mixedSlots = document.getElementsByClassName('btnMixed')
+
+for (let carSlot of carSlots) {
+    carSlot.addEventListener('click', slotBooked, console.log('click'))
+
+    function slotBooked() {
+        carSlot.style.backgroundColor = 'orange'
+        carSlot.innerHTML = '<h2>Slot Booked!</h2>'
+
+        setTimeout(
+            () => {
+                carSlot.style.backgroundColor = 'orange'
+            }, 50000)
+    }
+}
+
+for (let mixedSlot of mixedSlots) {
+    mixedSlot.addEventListener('click', slotBooked)
+
+    function slotBooked() {
+        mixedSlot.style.backgroundColor = 'orange'
+        mixedSlot.innerHTML = '<h2>Slot Booked!</h2>'
+
+        setTimeout(
+            () => {
+            mixedSlot.previousElementSibling.style.display= 'block'
+        }, 5000);
+    }
+}
